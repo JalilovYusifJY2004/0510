3444ae9838d76c0307b0ff9181a837cb6fc74a9a
JalilovYusifJY2004 <yusifcelilli2004@gmail.com>
2023-10-04 22:44:50 +0400

    add file

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Security.AccessControl;
+
+namespace task0510
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //int[] array = { 7, 2, 9, 4, 5, };
+            //int min = Min(array);
+            //Console.WriteLine(min);
+
+            //task2
+            //int cevreR = 5;
+            //int duzbucaqa = 5;
+            //int duzbucaqb = 6;
+            //int parapipLength = 5;
+            //int parapipWidth = 7;
+            //int ucbucaqA = 8;
+            //int ucbucaqB = 9;
+            //int ucbucaqC = 10;
+
+            //Console.WriteLine("duzbucaq sahe:" + Area(duzbucaqa, duzbucaqb));
+
+            //task3
+
+            //Console.WriteLine("x = ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.WriteLine("y = ");
+            //int y = int.Parse(Console.ReadLine());
+            
+        }
+        //task1
+        //public static int Min(int[] array)
+        //{
+        //    int min = array[0];
+        //    for (int i = 1; i < array.Length; i++)
+        //    {
+        //        if (array[i] < min) min = array[i];
+        //    }
+        //    return min;
+        //}
+        
+
+        //task2
+    //    const int pi = 3;
+    //    public static int Area(int r)
+    //    {
+    //        return  pi * r *r;
+    //    }
+    //public static int Area(int a, int b)
+    //    {
+    //        return a * b;
+    //    }
+    //    public  static int Area(int a, int b, int c)
+
+    //    {
+    //        return 2* (a * b + a * c + b * c);
+
+    //    }
+        
+    //   public static int Area(int p, int r, int a, int b, int c)
+    //    {
+    //        return p * r; p = (a + b + c) / 2;
+    //    }
+         
+        //task3
+
+      //  public static int Positive(int x, int y) 
+      //  {
+      //      if (x < 0)
+      //          if (y > 0)
+      //              return y;
+      //  }
+      //public static int Negative(int x, int y)
+      //  {
+      //      if (y < 0)
+      //          if (x > 0)
+      //              return x;
+      //  }
+      
+    }
+}
